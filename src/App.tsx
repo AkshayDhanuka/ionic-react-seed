775fd065aa4dd0ed1191465e565293d5ed3c72a8
AkshayDhanuka <kdhanuka26@gmail.com>
2019-05-29 22:57:53 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  IonApp,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import React, { Component } from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    add: 0,
+  };
+  subtract = () => {
+    if (this.state.add > 0) {
+      this.setState({
+        add: this.state.add -= 1,
+      });
+    }
+  };
+  addition = () => {
+    this.setState({
+      add: this.state.add += 1,
+    });
+  };
+
+  render() {
+    return (
+      <IonApp>
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonTitle>HomePage</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>Counter App</IonCardSubtitle>
+              <IonCardTitle>
+                Total Value {this.state.add}
+              </IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonButton onClick={this.addition}>Add</IonButton>
+              <IonButton onClick={this.subtract}>Subtract</IonButton>
+            </IonCardContent>
+          </IonCard>
+        </IonContent>
+      </IonApp>
+    );
+  }
 }
 
 export default App;
